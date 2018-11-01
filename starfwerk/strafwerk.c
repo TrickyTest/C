60377d1a293f52ceca84e13505fc3e6926a972f6
Jeroen P. Broks <facebook@tbbs.nl>
2018-11-01 16:27:48 +0100

    Nette straf

--- starfwerk/strafwerk.c
+++ starfwerk/strafwerk.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-  for (int i=0;i<1000;i++) printf("%3d: Ik mag geen kauwgom onder tafel plakken!\n",i+1);
+  for (int i=0;i<1000;i++) printf("%5d: Ik mag geen kauwgom onder tafel plakken!\n",i+1);
   return 0;
 }
